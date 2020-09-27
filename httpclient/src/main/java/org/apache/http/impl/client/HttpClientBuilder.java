a35792b5123b096912cf8e495e03de39df8c57d2
heyuanxin3 <heyuanxin3@jd.com>
2020-09-27 20:30:17 +0800

    c

--- httpclient/src/main/java/org/apache/http/impl/client/HttpClientBuilder.java
+++ httpclient/src/main/java/org/apache/http/impl/client/HttpClientBuilder.java
@@ -157,6 +157,10 @@ import org.apache.http.util.VersionInfo;
 public class HttpClientBuilder {
 
     private HttpRequestExecutor requestExec;
+    /**
+     * 在握手期间，如果URL的主机名和服务器的标识主机名不匹配，则验证机制可以回调此接口实现
+     * 程序来确定是否应该允许此连接，如果回调内实现不恰当，默认接受所有域名，则有安全风险。
+     */
     private HostnameVerifier hostnameVerifier;
     private LayeredConnectionSocketFactory sslSocketFactory;
     private SSLContext sslContext;
